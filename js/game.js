d67c186d3f4443396ffd8dc4f83c926fa12ab9a1
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-11-08 19:38:41 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -7,14 +7,14 @@
 // This is the Phaser3 game configuration file
 
 /**
-* Start Phaser Game.
-*/
+ * Start Phaser Game.
+ */
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
-backgroundColor: 0x5f6e7a
-}
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
