3eb9d454644864f34c8b616b11d6e97926c973fb
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-10 19:20:15 +0000

    correct index and js

--- js/game.js
+++ js/game.js
@@ -6,15 +6,25 @@
 // Created on: Nov 8 2022
 // This is the Phaser3 game configuration file
 
-/**
- * Start Phaser Game.
- */
+//* Game score */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
   backgroundColor: 0x5f6e7a,
-};
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
 
-const game = new Phaser.Game(config);
-console.log(game);
+const game = new Phaser.Game(config)
+console.log(game)
