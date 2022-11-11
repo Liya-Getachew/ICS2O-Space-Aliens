22a3aed020174d6d7585558ce20635a649729cf3
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-11-11 01:01:41 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-  }
-  
-  const game = new Phaser.Game(config)
-  console.log(game)
+  backgroundColor: 0x5f6e7a,
+};
+
+const game = new Phaser.Game(config);
+console.log(game);
